7b15d680a3985769d1abd4213a407f36e7a89e05
afunc <afunc@sina.com>
2024-11-11 20:16:53 +0800

    adjust code

--- src/Komiic/PageViewModels/INavigationAware.cs
+++ src/Komiic/PageViewModels/INavigationAware.cs
@@ -4,7 +4,15 @@ namespace Komiic.PageViewModels;
 
 public interface INavigationAware
 {
+    /// <summary>
+    /// 导航到本页
+    /// </summary>
+    /// <returns></returns>
     Task NavigatedTo();
-
+    
+    /// <summary>
+    /// 离开本页
+    /// </summary>
+    /// <returns></returns>
     Task NavigatedFrom();
 }
